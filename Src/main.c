afcb2b38ab8f74ce9f23bff595279fae69a89919
ben <limhanyangb@gmail.com>
2019-01-31 14:14:08 -0600

    best counter value is 1000

--- Src/main.c
+++ Src/main.c
@@ -147,9 +147,9 @@ int main(void)
 		  if(userTime.Hours==14){
 			  //Wakey Wakey dingdong
 			  if (pwm_flag==0){
-				  setPWM(htim1,TIM_CHANNEL_2,500,250);
+				  setPWM(htim1,TIM_CHANNEL_2,500,100);
 				  pwm_flag = 1;
-				  HAL_Delay(10); // not sure why the light is so dim...
+				  //HAL_Delay(10); // not sure why the light is so dim...
 			  }
 			  //HAL_Delay(10);
 
@@ -290,7 +290,7 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 2000;
+  htim1.Init.Prescaler = 1000;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 500;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
